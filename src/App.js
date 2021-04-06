5167f76c2a36444ad1d80bd6e67270371dd091a6
MaryAnn Alexander <malexander@catalyte.io>
2021-04-06 14:16:05 -0700

    page view not rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import countapi from 'countapi-js';
 
 countapi.visits().then((result) => {
   console.log(result.value);
 });
+countapi.get('https://api.countapi.xyz/get/purplecow/1ccb732e-b55a-4404-ad3f-0f99c02fe44e', 'test').then((result) => {
+  return result
+});
+countapi.hit('https://api.countapi.xyz/update/purplecow/1ccb732e-b55a-4404-ad3f-0f99c02fe44e/?amount=1', 'visits').then((result) => {
+  return result
+});
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(null);
+  const [clicked, setClicked] = useState(false);
+  const baseURL = "https://api.countapi.xyz/get/purplecow/1ccb732e-b55a-4404-ad3f-0f99c02fe44e"
+
+  const fetchData = async () => {
+    setClicked(true);
+    const response = await countapi.get(baseURL, 'test').then((result) => {
+      return result
+    });
+    setCount(response.data.value);
+}
+
   return (
     <div className="App">
       <header className="App-header">
 
-        <p>Page View Count</p>
+        <h1>Page View Count</h1>
 
-        <div id="visits">...</div>
+        <div id="visits">{count}</div>
 
-        <button onclick="clicked()">Hit API</button>
+        <button onclick={fetchData}>Hit API</button>
 
       </header>
     </div>
